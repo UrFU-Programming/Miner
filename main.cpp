b37d055df90830d85b72a3ee273da365e5461faa
Vadim Chetkov <vadim.chetkov@gmail.com>
2016-04-29 20:22:40 +0500

    Registering QML type
    Task 20.2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include "MainWindow.hpp"
 #include <QApplication>
 
+#include <QtQml>
+
+#include "Field.hpp"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    qmlRegisterType<Field>("GameComponents", 1, 0, "Field");
+
     MainWindow w;
     w.show();
 
